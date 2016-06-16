e3401b5e41a6ba5ea7b7ac82bd99549af854bf6d
Ruben Harms <postbus@rubenharms.nl>
2016-06-16 15:33:22 +0200

    Added configuration

--- DependencyInjection/GenderizeIoExtension.php
+++ DependencyInjection/GenderizeIoExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Class GenderizeIoExtension
@@ -22,5 +23,20 @@ class GenderizeIoExtension extends Extension
         // load services
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $config = array_merge(['api_key' => '', 'endpoint' => 'http://api.genderize.io/'], $config);
+
+       
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->getDefinition('genderize_io.client')->addArgument($config['endpoint']);
+
+        if(!empty($config['api_key']))
+            $container->getDefinition('genderize_io.genderizer')->addMethodCall('setApiKey',$config['api_key']);
+
     }
 }
